851e272846f7cdb747dd9e5a1193279db9fb7fbb
JeremyXa <jeremyhv2000@gmail.com>
2025-09-22 02:12:09 -0500

    Este es el codigo fuente Base
    Este es código java que implementa MVC

---
+++ src/mvc/in/java/jeremy/MVCInJavaJeremy.java
@@ -0,0 +1,19 @@
+
+package mvc.in.java.jeremy;
+import controllers.HomeController;
+/**
+ *
+ * @author jerem
+ */
+public class MVCInJavaJeremy {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        HomeController c = new HomeController();
+        
+    }
+    
+}
